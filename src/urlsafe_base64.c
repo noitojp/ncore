a2bf9bb4bda7d5a5873ede9898843ff2dded975d
noito <noitojp@gmail.com>
2019-11-14 18:07:28 +0000

    update urlsafe base64

--- src/urlsafe_base64.c
+++ src/urlsafe_base64.c
@@ -1,127 +1,93 @@
-#include <sys/types.h>
-#include <stdint.h>
-#include <string.h>
 #include "urlsafe_base64.h"
 
-static inline char _conv2char(uint8_t ch)
+static const char _basis_64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+static const unsigned char _pr2six[256] = {
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 62, 64, 63,
+	52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64,
+	64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+	15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 63,
+	64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+	41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
+};
+
+int nax_encode64_urlsafe(char *dest,const char *src,int slen)
 {
-	static const char _conv_arr[] = {
-		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-		'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-		'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-		'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-		'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-		'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-		'w', 'x', 'y', 'z', '0', '1', '2', '3',
-		'4', '5', '6', '7', '8', '9', '-', '_'
-	};
-
-	return( _conv_arr[ch] );
-}
-
-static inline uint8_t _conv2bit(uint8_t *d,char ch)
-{
-	if( ch >= 'A' && ch <= 'Z' ){
-		*d = ch - 'A';
-	}
-	else if( ch >= 'a' && ch <= 'z' ){
-		*d = ch - 'a' + 0x1a;
-	}
-	else if( ch >= '0' && ch <= '9' ){
-		*d = ch - '0' + 0x34;
-	}
-	else if( '-' == ch ){
-		*d = 0x3e;
-	}
-	else if( '_' == ch ){
-		*d = 0x3f;
-	}
-	else{
-		return(0);
-	}
-
-	return(1);
-}
-
-int nax_encode64_urlsafe(char *dest,const char *src,int srclen)
-{
+	char *d;
+	const unsigned char *s;
 	int ix;
-	int jx;
 
-	if(NULL == dest || NULL == src){
-		return(-1);
+	if(NULL == dest || NULL == src || slen <= 0){
+		return 0;
 	}
 
-	for( ix = 0, jx = 0; ix < srclen; ix += 3, jx += 4 ){
-		dest[jx] = _conv2char((uint8_t)src[ix] >> 2);
-
-		if( ix + 1 >= srclen ){
-			dest[jx + 1] = _conv2char(((uint8_t)src[ix] & 0x03) << 4);
-			dest[jx + 2] = '\0';
-			return(jx + 2);
-		}
-
-		dest[jx + 1] = _conv2char((((unsigned char)src[ix] & 0x03) << 4) + (((unsigned char)src[ix + 1]) >> 4));
-		if( ix + 2 >= srclen ){
-			dest[jx + 2] = _conv2char(((unsigned char)src[ix + 1] & 0x0f) << 2);
-			dest[jx + 3] = '\0';
-			return(jx + 3 );
-		}
-
-		dest[jx + 2] = _conv2char((((unsigned char)src[ix + 1] & 0x0f) << 2) + (((unsigned char)src[ix + 2]) >> 6));
-		dest[jx + 3] = _conv2char((unsigned char)src[ix + 2] & 0x3f);
-		dest[jx + 4] = '\0';
+	d = dest;
+	s = (const unsigned char*)src;
+	for( ix = 0; ix < slen - 2; ix += 3 ){
+		*d++ = _basis_64[(s[ix] >> 2) & 0x3f];
+		*d++ = _basis_64[((s[ix] & 0x3) << 4) | ((int)(s[ix + 1] & 0xf0) >> 4)];
+		*d++ = _basis_64[((s[ix + 1] & 0xf) << 2) | ((int)(s[ix + 2] & 0xc0) >> 6)];
+		*d++ = _basis_64[s[ix + 2] & 0x3f];
 	}
 
-	return(jx);
+	if( ix < slen ){
+		*d++ = _basis_64[(s[ix] >> 2) & 0x3f];
+		if( (slen - 1) == ix ){
+			*d++ = _basis_64[((s[ix] & 0x3) << 4)];
+		}
+		else{
+			*d++ = _basis_64[((s[ix] & 0x3) << 4) | ((int)(s[ix + 1] & 0xf0) >> 4)];
+			*d++ = _basis_64[((s[ix + 1] & 0xf) << 2)];
+		}
+	}
+	*d = '\0';
+	return d - dest;
 }
 
-int nax_decode64_urlsafe(uint8_t *dest,const char *src,int srclen)
+int nax_decode64_urlsafe(unsigned char *dest,const char *src,int slen)
 {
-	int rc;
+	int rc = 0;
 	int ix;
-	int jx;
-
-	if( NULL == dest || NULL == src ){
-		return(0);
+	
+	if(NULL == dest || NULL == src || slen <= 0){
+		return 0;
 	}
 
-	for( ix = 0, jx = 0; ix < srclen; ix += 4, jx += 3 ){
-		uint8_t tmp[4];
-
-		memset(tmp,'\0',sizeof(tmp));
-		if( !_conv2bit(&tmp[0],(char)src[ix]) ){ return(0); }
-		if( srclen > ix + 1 ){
-			if( !_conv2bit(&tmp[1],(char)src[ix+1]) ){ return(0); }
-		}
-		if( srclen > ix + 2 ){
-			if( !_conv2bit(&tmp[2],(char)src[ix+2]) ){ return(0); }
-		}
-		if( srclen > ix + 3 ){
-			if( !_conv2bit(&tmp[3],(char)src[ix+3]) ){ return(0); }
-		}
-
-		if( ix + 3 < srclen ){
-			dest[jx] = (tmp[0] << 2) + (tmp[1] >> 4);
-			dest[jx + 1] = (tmp[1] << 4) + (tmp[2] >> 2);
-			dest[jx + 2] = (tmp[2] << 6) + tmp[3];
-		}
-		else if( ix + 2 < srclen ){
-			dest[jx] = (tmp[0] << 2) + (tmp[1] >> 4);
-			dest[jx + 1] = (tmp[1] << 4) + (tmp[2] >> 2);
-			dest[jx + 2] = tmp[2] << 6;
-		}
-		else if( ix + 1 < srclen ){
-			dest[jx] = (tmp[0] << 2) + (tmp[1] >> 4);
-			dest[jx + 1] = tmp[1] << 4;
-		}
-		else if( ix < srclen ){
-			dest[jx] = tmp[0] << 2;
-		}
+	while( slen >= 4 ){
+		*dest++ = (unsigned char)(_pr2six[src[0]] << 2 | _pr2six[src[1]] >> 4);
+		*dest++ = (unsigned char)(_pr2six[src[1]] << 4 | _pr2six[src[2]] >> 2);
+		*dest++ = (unsigned char)(_pr2six[src[2]] << 6 | _pr2six[src[3]]);
+		slen -= 4;
+		src += 4;
+		rc += 3;
 	}
 
-	rc = ((srclen + 3) / 4) * 3;
-    rc -= (4 - srclen) & 3;
-	return(rc);
+	switch( slen ){
+	case 1:
+		*dest++ = (unsigned char)(_pr2six[src[0]] << 2);
+		break;
+	case 2:
+		*dest++ = (unsigned char)(_pr2six[src[0]] << 2 | _pr2six[src[1]] >> 4);
+		*dest++ = (unsigned char)(_pr2six[src[1]] << 4);
+		rc += 1;
+		break;
+	case 3:
+		*dest++ = (unsigned char)(_pr2six[src[0]] << 2 | _pr2six[src[1]] >> 4);
+		*dest++ = (unsigned char)(_pr2six[src[1]] << 4 | _pr2six[src[2]] >> 2);
+		*dest++ = (unsigned char)(_pr2six[src[2]] << 6);
+		rc += 2;
+		break;
+	}
+
+	return rc;
 }
 
